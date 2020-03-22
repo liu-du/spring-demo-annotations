21b4d325dd0c5bb3c7237bedeeb99e4fd20b0077
Jimmy Liu <jliu5@woolworths.com.au>
2020-03-22 12:06:59 +1100

    inject values from properties file using java annotation

--- src/com/example/springdemo/TennisCoach.java
+++ src/com/example/springdemo/TennisCoach.java
@@ -2,17 +2,24 @@ package com.example.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
     @Autowired
     @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     @Override
     public String getDailyWorkout() {
-        return "Practice your backhand volley";
+        return team + ": Practice your backhand volley. email to: " + email + " once you finish.";
     }
 
     @Override
